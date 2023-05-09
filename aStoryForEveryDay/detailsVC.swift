70927e5ec158cdb342a97a52d9e14b9d72feaa1e
Sevda Abbasi <sevdaabbasi37@gmail.com>
2023-05-09 11:33:14 +0300

    first commit

---
+++ aStoryForEveryDay/detailsVC.swift
@@ -0,0 +1,34 @@
+//
+//  detailsVC.swift
+//  aStoryForEveryDay
+//
+//  Created by Sevda Abbasi on 5.05.2023.
+//
+
+import UIKit
+
+class detailsVC: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var labelText: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+   
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
